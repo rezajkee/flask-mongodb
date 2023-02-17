76e60c6175d3f9111bf9a75d04e37ea9b8611e8d
Anton Shefer <rezajkee@mail.ru>
2023-02-17 19:15:41 +0500

    Implement basic functionality

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+habits = ["Test habit", "Test 2 mate"]
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
