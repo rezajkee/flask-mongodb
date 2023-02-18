0b1c88de23163a0a7e884c346d581e3d0992ac8b
Anton Shefer <rezajkee@mail.ru>
2023-02-18 22:18:42 +0500

    Implement completing habits, refactor to flask blueprint, set up MongoDB

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-import datetime
-from flask import Flask, render_template, request
+import os
+from flask import Flask
+from routes import pages
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-habits = ["Test habit", "Test 2 mate"]
-
-
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        dates = [start + datetime.timedelta(days=diff)
-                 for diff in range(-3, 4)]
-        return dates
-    return {"date_range": date_range}
+load_dotenv()
 
 
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home", selected_date=selected_date)
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client.tracker
 
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habits.append(request.form.get("habit"))
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
+    app.register_blueprint(pages)
+    return app
